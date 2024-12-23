2167c1bc0772cbf729e55e6987d0ecf5e534d50a
Frieda (Jingying) Huang <jingyingfhuang@gmail.com>
2024-12-23 12:07:08 -0800

    Improve _build_tag logic

--- src/codebase_parser/ast_graph_builder.py
+++ src/codebase_parser/ast_graph_builder.py
@@ -1,40 +1,91 @@
+import os
+from typing import Generator, List, Optional
+
 import networkx as nx
+from codebase_parser.schemas import Tag, TagKind, TagRole
+from commons import PY_LANGUAGE, ASTMapping, FilepathType, project_paths
+from loguru import logger
+from tree_sitter import Tree
 
 
 class ASTGraphBuilder:
     def __init__(self) -> None:
-        self.tags = []
+        self.tags: List[Tag] = []
 
-    def build_tags(self, ast_tree: dict) -> list:
-        for filepath, parsed_tree in list(ast_tree.items()):
-            captures = query.captures(parsed_tree.root_node)
-            for node, tag in captures.items():
-                for t in tag:
-                    capture_parts = str(node).split(".")
+    def build_tags(self, ast_tree: ASTMapping) -> List[Tag]:
+        """Build tags for all ASTs in the codebase."""
+        for filepath, tree in ast_tree.items():
+            logger.info(f"Building tags for {filepath}")
+            tags = list(self._build_tag(filepath, tree))
+            self.tags.extend(tags)
+        return self.tags
 
-                    role = (
-                        TagRole.DEFINITION
-                        if "definition" in capture_parts
-                        else TagRole.REFERENCE
-                    )
-                    kind = next(
-                        (
-                            part
-                            for part in capture_parts
-                            if part in ["class", "function", "call"]
-                        )
-                    )
+    def build_graph_from_ast(self, ast_tree: ASTMapping) -> nx.MultiDiGraph:
+        pass
 
-                    rel_filename = os.path.relpath(filepath)
-                    tag = Tag(
-                        name=t.text.decode("utf-8"),
-                        kind=kind,
-                        role=role,
-                        line=t.start_point[0],
-                        rel_filename=rel_filename,
-                        abs_filename=filepath,
-                    )
-                    self.tags.append(tag)
+    def _get_role(self, capture_name: str) -> Optional[TagRole]:
+        """Get the role of an entity in the code.
 
-    def build_graph_from_ast(self, ast_tree: dict) -> nx.MultiDiGraph:
-        self.build_tags(ast_tree)
+        role: definition or reference
+        """
+        capture_roles = {
+            "name.definition.": TagRole.DEFINITION,
+            "name.reference.": TagRole.REFERENCE,
+        }
+        return next(
+            (
+                role
+                for prefix, role in capture_roles.items()
+                if capture_name.startswith(prefix)
+            ),
+            None,
+        )
+
+    def _get_kind(self, capture_parts: List[str]) -> TagKind:
+        """Return the kind of an entity in the code.
+
+        kind: class, function, or call
+        """
+        return next(
+            part
+            for part in capture_parts
+            if part in [TagKind.CLASS, TagKind.FUNCTION, TagKind.CALL]
+        )
+
+    def _build_tag(
+        self, filepath: FilepathType, tree: Tree
+    ) -> Generator[Tag, None, None]:
+        """Build a tag based on a filepath and its corresponding AST."""
+        query_string = self.read_query_file()
+        query = PY_LANGUAGE.query(query_string)
+        captures = query.captures(tree.root_node)
+
+        for capture_name, nodes in captures.items():
+            for node in nodes:
+                role = self._get_role(capture_name)
+                if role is None:
+                    continue
+
+                # Convert capture_name to hierarchical parts
+                # e.g., "name.definition.class" -> ['name', 'definition', 'class']
+                capture_parts = str(capture_name).split(".")
+                kind = self._get_kind(capture_parts)
+                rel_filename = os.path.relpath(filepath)
+                name = node.text.decode("utf-8")
+
+                logger.debug(name)
+
+                yield Tag(
+                    name=name,
+                    kind=kind,
+                    role=role,
+                    start_line=node.start_point[0],
+                    rel_filename=rel_filename,
+                    abs_filename=filepath,
+                )
+
+    @staticmethod
+    def read_query_file() -> str:
+        """Load and parse a Tree-sitter query specifically for Python code."""
+        query_path = project_paths.QUERIES / "tree-sitter-python-tags.scm"
+        return query_path.read_text()
