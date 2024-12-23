dc8af1ab2867a5f7a58eeaf65e997bcde299cf34
Frieda (Jingying) Huang <jingyingfhuang@gmail.com>
2024-12-22 23:28:28 -0800

    Added build_tags

--- src/codebase_parser/ast_graph_builder.py
+++ src/codebase_parser/ast_graph_builder.py
@@ -3,7 +3,38 @@ import networkx as nx
 
 class ASTGraphBuilder:
     def __init__(self) -> None:
-        pass
+        self.tags = []
+
+    def build_tags(self, ast_tree: dict) -> list:
+        for filepath, parsed_tree in list(ast_tree.items()):
+            captures = query.captures(parsed_tree.root_node)
+            for node, tag in captures.items():
+                for t in tag:
+                    capture_parts = str(node).split(".")
+
+                    role = (
+                        TagRole.DEFINITION
+                        if "definition" in capture_parts
+                        else TagRole.REFERENCE
+                    )
+                    kind = next(
+                        (
+                            part
+                            for part in capture_parts
+                            if part in ["class", "function", "call"]
+                        )
+                    )
+
+                    rel_filename = os.path.relpath(filepath)
+                    tag = Tag(
+                        name=t.text.decode("utf-8"),
+                        kind=kind,
+                        role=role,
+                        line=t.start_point[0],
+                        rel_filename=rel_filename,
+                        abs_filename=filepath,
+                    )
+                    self.tags.append(tag)
 
     def build_graph_from_ast(self, ast_tree: dict) -> nx.MultiDiGraph:
-        pass
+        self.build_tags(ast_tree)
